d44d2b87055365536cb4f8bf957fd688d2687b15
Brayan <smithgaspri@gmail.com>
2021-10-04 11:21:59 -0500

    Update de círculo, custom permissions y DRF Mixins

--- cride/circles/serializers/circles.py
+++ cride/circles/serializers/circles.py
@@ -1,4 +1,4 @@
-"""Circles serializers"""
+"""Circles model serializers"""
 
 # Django REST Framework
 from rest_framework import serializers
@@ -8,6 +8,12 @@ from cride.circles.models import Circle
 
 class CircleModelSerializer(serializers.ModelSerializer):
     """circle model serializer"""
+    members_limit = serializers.IntegerField(
+        required= False,
+        min_value= 10,
+        max_value= 32000
+    )
+    is_limited = serializers.BooleanField(default=False)
 
     class Meta:
         """Meta class"""
@@ -19,3 +25,18 @@ class CircleModelSerializer(serializers.ModelSerializer):
             'verified', 'is_public',
             'is_limited', 'members_limit'
         )
+        read_only_fields = (
+            'is_public', 
+            'verified',
+            'rides_taken',
+            'rides_offered'
+        )
+
+
+    def validate(self,data):
+        """Ensure both members_limit and is_limit are present"""
+        members_limit = data.get('members_limit', None)
+        is_limited = data.get('is_limited', False)
+        if is_limited ^ bool(members_limit):#Si una es False y la otra True, entra a la excepcion
+            raise serializers.ValidationError('If circle is limited, a member limit must be provided')
+        return data
--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -1,16 +1,53 @@
 """Circle views"""
 
 # Django REST Framework
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
+from rest_framework import permissions
+
+# Permissions
+from rest_framework.permissions import IsAuthenticated
+from cride.circles.permissions.circles import IsCircleAdmin
 
 #Serializers
 from cride.circles.serializers import CircleModelSerializer
 
 #Models
-from cride.circles.models import Circle
+from cride.circles.models import Circle,Membership
 
-class CircleViewSet(viewsets.ModelViewSet):
+class CircleViewSet(mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.ListModelMixin,
+                   viewsets.GenericViewSet):
     """Circle view set"""
 
-    queryset = Circle.objects.all()
     serializer_class= CircleModelSerializer
+    
+
+    def get_queryset(self):
+        """Restrict list to public only"""
+        queryset = Circle.objects.all()
+        if self.action == 'list':
+            queryset = Circle.objects.filter(is_public=True)
+        return queryset
+
+    def get_permissions(self):
+        """Assign permissions base on a action"""
+        permissions =[IsAuthenticated]
+        if self.action in ['update', 'partial_update']:
+            permissions.append(IsCircleAdmin)
+        return [permission() for permission in permissions]
+
+
+    def perform_create(self, serializer):
+        """Assign circle admin."""
+        circle = serializer.save()
+        user = self.request.user
+        profile = user.profile
+        Membership.objects.create(
+            user=user,
+            profile=profile,
+            circle=circle,
+            is_admin=True,
+            remaining_invitations=10
+        )
