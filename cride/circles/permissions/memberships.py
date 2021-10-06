52d4a17d9d24c10bd6f1de2d3c6d7b94b3db9ab3
Brayan <smithgaspri@gmail.com>
2021-10-06 16:33:10 -0500

    Modelo de invitaciones y manager

---
+++ cride/circles/permissions/memberships.py
@@ -0,0 +1,28 @@
+"""Circles permission classes"""
+# Django REST Framework
+from rest_framework.permissions import BasePermission
+
+# Models
+from cride.circles.models import Membership
+
+
+class IsActiveCircleMember(BasePermission):
+    """Allow acces only to circle members
+    
+    Expect that views implementing this permission
+    have a 'circle' attribute assigned
+    """
+
+    def has_permission(self, request, view):
+        """Verify if user is a active member of the circle"""
+
+        try:
+            Membership.objects.get(
+                user = request.user,
+                circle = view.circle,
+                is_active = True
+            )
+        except Membership.DoesNotExist:
+            return False
+        return True
+
