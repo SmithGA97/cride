2c49edf7e2554751169794b29bc0ed8f09cc8e4b
Brayan <smithgaspri@gmail.com>
2021-10-10 17:11:31 -0500

    Listado de rides

--- cride/rides/serializers/rides.py
+++ cride/rides/serializers/rides.py
@@ -5,10 +5,24 @@ from rest_framework import serializers
 
 # Models
 from cride.rides.models import Ride
+from cride.circles.models import Membership
 
 # Utilities
 from datetime import timedelta
 from django.utils import timezone
+
+class RideModelSerializer(serializers.ModelSerializer):
+    """Ride model serializer"""
+    class Meta:
+        """Meta class"""
+
+        model = Ride
+        fields = '__all__'
+        read_only_fields = (
+            'offered_by',
+            'offered_in',
+            'rating'
+        )
 
 class CreateRideSerializer(serializers.ModelSerializer):
     """Create ride serializer."""
@@ -26,10 +40,57 @@ class CreateRideSerializer(serializers.ModelSerializer):
 
     def  validate_departure_date(self, data):
         """Verify date is not in the past"""
-        min_date = timezone.now() + timedelta(minutes=30)
+        min_date = timezone.now() + timedelta(minutes=10)
 
         if data < min_date:
             raise serializers.ValidationError(
                 'Departure time must be at least pass the next 20 min window'
             )
         return data
+
+    def validate(self, data):
+        """Validate.
+
+        Verify that the person who offer the ride is member and
+        also the same user making the request
+        """
+        if self.context['request'].user != data['offered_by']:
+            raise serializers.ValidationError('Rides offered on behalf of others are not allowed')
+        
+        user = data['offered_by']
+        circle =self.context['circle']
+        try:
+            membership = Membership.objects.get(
+                user=user, 
+                circle=circle, 
+                is_active=True
+            )
+        except Membership.DoesNotExist:
+            raise serializers.ValidationError('User is not an active member of the circle')
+
+        if data['arrival_date'] <= data['departure_date']:
+            raise serializers.ValidationError('Departure date must be happen after arrival date')
+        self.context['membership'] = membership
+        
+        return data
+
+    def create(self, data):
+        """Create ride and update stats"""
+        circle = self.context['circle']
+        ride = Ride.objects.create(**data, offered_in=circle)
+
+        # Circle
+        circle.rides_offered += 1
+        circle.save()
+
+        # Membership
+        membership = self.context['membership']
+        membership.rides_offered += 1
+        circle.save()
+
+        # Profile
+        profile = data['offered_by'].profile
+        profile.rides_offered +=1
+        profile.save()
+
+        return ride
--- cride/rides/views/rides.py
+++ cride/rides/views/rides.py
@@ -8,22 +8,57 @@ from rest_framework.generics import get_object_or_404
 from cride.circles.permissions.memberships import IsActiveCircleMember
 from rest_framework.permissions import IsAuthenticated
 
+# Filters
+from rest_framework.filters import SearchFilter, OrderingFilter
 # Serializers
-from cride.rides.serializers import CreateRideSerializer
+from cride.rides.serializers import (
+    CreateRideSerializer,
+    RideModelSerializer
+    )
 
 # Models
 from cride.circles.models import Circle
 
-class RideViewSet(mixins.CreateModelMixin,
+# Utilities
+from datetime import timedelta
+from django.utils import timezone
+
+class RideViewSet(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
                   viewsets.GenericViewSet):
     """Ride view set"""
 
     serializer_class = CreateRideSerializer
-    permission_classess = [IsAuthenticated, IsActiveCircleMember]
+    permission_classes = [IsAuthenticated, IsActiveCircleMember]
+    filter_backends = (SearchFilter, OrderingFilter)
+    ordering = ('departure_date', 'arrival_date', 'available_seats')
+    ordering__fields = ('departure_date', 'arrival_date', 'available_seats')
+    search_fields = ('departure_location', 'arrival_location')
 
     def dispatch(self, request, *args, **kwargs): #Con esto apenas se instancie se realiza la verificacion del circulo, en este caso el slug_name
             """"Verify that the circle exists"""
             slug_name = kwargs['slug_name']
             self.circle = get_object_or_404(Circle, slug_name = slug_name )
             return super(RideViewSet, self).dispatch(request, *args, **kwargs)
+
+    def get_serializer_context(self):
+        """Add circle to serializer context"""
+        context = super(RideViewSet, self).get_serializer_context()
+        context['circle'] = self.circle
+        return context
+
+    def get_serializer_class(self):
+        """Return serializer based on action"""
+        if self.action == 'create':
+            return CreateRideSerializer
+        return RideModelSerializer
+
+    def get_queryset(self):
+        """Return active circle's ride"""
+        offset = timezone.now()+ timedelta(minutes=10)
+        return self.circle.ride_set.filter(
+            departure_date__gte=offset,
+            is_active=True,
+            available_seats__gte=1
+        )
 
