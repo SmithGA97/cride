b1fab88b16f9bbfa0b026300e03c718ea9ca73b5
Brayan <smithgaspri@gmail.com>
2021-10-11 10:31:27 -0500

    Uniser a viaje

--- cride/rides/permissions/rides.py
+++ cride/rides/permissions/rides.py
@@ -10,3 +10,12 @@ class IsRideOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
         """Verify requesting user in the ride creator"""
         return request.user == obj.offered_by
+
+class IsNotRideOwner(BasePermission):
+    """"Verify the passenger is not owner of ride"""
+
+    message = "The ride owner can't be a passenger."
+    def has_object_permission(self, request, view, obj):
+        """Check request user doesn't equal to obj"""
+
+        return request.user != obj.offered_by
